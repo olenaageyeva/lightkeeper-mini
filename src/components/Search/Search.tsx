e49120290b1a31ccbb3ab49047f1310bbf37a338
olenaageyeva <olenaageyeva@gmail.com>
2024-03-24 20:15:15 -0400

    handle lowercase

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -27,14 +27,19 @@ export const Search = () => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setSearchParams({ ticker: term })
-        setSearchTerm(term);
+        setSearchTerm(term.toUpperCase());
     }
-    return <form onSubmit={handleSubmit} className="flex relative my-4 xl:basis-40 shrink-0" >
+    return <form onSubmit={handleSubmit} className="flex relative my-4 xl:basis-40 shrink-0 pr-4" >
         <button className="absolute left-3 top-5 md:top-1/2">
             <svg width="20" height="20" fill="currentColor" className="-mt-2.5 text-gray-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
                 <path fillRule="evenodd" clipRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
             </svg>
         </button>
-        <input className="focus:ring-2 focus:ring-blue-500 focus:outline-none w-full text-sm leading-6 text-gray-900 placeholder-gray-400 rounded-md py-2 pl-10 ring-1 ring-gray-200 shadow-sm " type="text" aria-label="Filter projects" placeholder="Enter a ticker..." value={term} onChange={(e) => setTerm(e.target.value)} />
+        <input className="focus:ring-2 focus:ring-blue-500 focus:outline-none w-full text-sm leading-6 text-gray-900 placeholder-gray-400 rounded-md py-2 pl-10 ring-1 ring-gray-200 shadow-sm "
+            type="text"
+            aria-label="Filter projects"
+            placeholder="Enter a ticker..."
+            value={term}
+            onChange={(e) => setTerm(e.target.value)} />
     </form >
 }
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,9 +1,9 @@
-import React, {useContext} from "react"
+import React, {useContext, FC} from "react"
 import { Header } from "../../components/Header/Header"
 import { Results } from "../../components/Results/Results"
 import { Context } from "../../components/Context/Context"
 
-export const SearchPage = () => {
+export const SearchPage:FC = () => {
     const {isDark}=useContext(Context);
 
     return <div key={isDark ? "dark" : ""} className="bg-white w-screen h-fit min-w-screen lg:min-w-min dark:bg-black min-h-130vh block">
